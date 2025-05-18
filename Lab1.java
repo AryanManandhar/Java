ce42ff878569eaeb2427ba2c0e3b23d62fce0d63
AryanManandhar <be2023ce204@gces.edu.np>
2025-05-18 10:55:38 +0545

    Update Lab1.java

--- Lab1.java
+++ Lab1.java
@@ -2,19 +2,19 @@
 import java.util.Scanner;
 public class Operation{
     public static void main(String[] args) {
-        float n1, n2, sum, dif, mul, div;
+        float n1, n2, sum, sub, mul, div;
         Scanner scanner = new Scanner(System.in);  
         System.out.println("Enter two numbers");
         n1 = scanner.nextInt();
         n2 = scanner.nextInt();
         sum = n1 + n2;
-        dif = n1 - n2;
+        sub = n1 - n2;
         mul = n1 * n2;
         div = n1 / n2;
-        System.out.println("Sum = " + sum);
-        System.out.println("Difference = " + diff);
-        System.out.println("Multiplication = " + mul);
-        System.out.println("Division = " + div);
+        System.out.println("Sum :" + sum);
+        System.out.println("Subtraction :" + sub);
+        System.out.println("Multiplication :" + mul);
+        System.out.println("Division :" + div);
         scanner.close();
     }
 }
